c8606965758829e430c0351513f9aaafac9895e9
Krystian <krystian@bpcoders.pl>
2021-03-12 15:26:03 +0100

    Add trainings table

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,11 +5,10 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
-            </div>
-        </div>
-    </div>
+    <section class="py-12">
+        Section reserved for calendar
+    </section>
+
+    <button>Add training</button>
+
 </x-app-layout>
