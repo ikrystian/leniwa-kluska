8d80db3badd217f28d74815b8792359e990c088b
Krystian <krystian@bpcoders.pl>
2021-03-14 18:59:08 +0100

    Remove id from training view

--- resources/views/livewire/training.blade.php
+++ resources/views/livewire/training.blade.php
@@ -1,7 +1,6 @@
 <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 my-4">
 @if($training->start)
     <div>
-        Training id: {{ $training->id }}<br>
         Training name: {{ $training->name  }}<br>
         Training date {{ $training->training_date }}<br>
     </div>
