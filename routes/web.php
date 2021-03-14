939e5d2f3b85904c570a8cd00d279156aacca9d5
Krystian <krystian@bpcoders.pl>
2021-03-14 12:40:29 +0100

    Add clear web route

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Livewire\Dashboard;
 use App\Http\Livewire\NewTraining;
 use App\Http\Livewire\TrainingViews;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -12,3 +13,12 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', Dashboard::class)->name('dashboard');
 Route::middleware(['auth:sanctum', 'verified'])->get('/trainings/{id}', NewTraining::class)->name('training');
 Route::middleware(['auth:sanctum', 'verified'])->get('/trainings/{id}/view', TrainingViews::class)->name('training');
+
+Route::get('/clear', function() {
+    Artisan::call('cache:clear');
+    Artisan::call('config:clear');
+    Artisan::call('config:cache');
+    Artisan::call('view:cache');
+    Artisan::call('key:generate');
+    return redirect('/login');
+});
